d11a61f4d3e06985a63c63021d090087a3075cbb
Singh <shailendra.4.singh@atos.net>
2016-12-22 09:52:07 +0530

    Created profile page and add logout button.

--- config/passport.js
+++ config/passport.js
@@ -45,27 +45,27 @@ module.exports = function(passport) {
 		});
 	}));
 
-	// passport.use('local-login', new LocalStrategy({
-	// 		usernameField: 'email',
-	// 		passwordField: 'password',
-	// 		passReqToCallback: true
-	// 	},
-	// 	function(req, email, password, done){
-	// 		process.nextTick(function(){
-	// 			User.findOne({ 'local.username': email}, function(err, user){
-	// 				if(err)
-	// 					return done(err);
-	// 				if(!user)
-	// 					return done(null, false, req.flash('loginMessage', 'No User found'));
-	// 				if(user.local.password != password){
-	// 					return done(null, false, req.flash('loginMessage', 'inavalid password'));
-	// 				}
-	// 				return done(null, user);
+	passport.use('local-login', new LocalStrategy({
+			usernameField: 'email',
+			passwordField: 'password',
+			passReqToCallback: true
+		},
+		function(req, email, password, done){
+			process.nextTick(function(){
+				User.findOne({ 'local.username': email}, function(err, user){
+					if(err)
+						return done(err);
+					if(!user)
+						return done(null, false, req.flash('loginMessage', 'No User found'));
+					if(user.local.password != password){
+						return done(null, false, req.flash('loginMessage', 'inavalid password'));
+					}
+					return done(null, user);
 
-	// 			});
-	// 		});
-	// 	}
-	// ));
+				});
+			});
+		}
+	));
 
 
 };
