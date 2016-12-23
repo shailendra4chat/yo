2f63903e5e311ea1709b6d02f0be49bb4c942004
shailendra4chat <shailendra.4.singh@atos.net>
2016-12-23 21:44:49 +0530

    Added bcrypt and social login [facebook and google] Signed-off-by: Shailendra Singh <shailendra4chat@gmail.com>

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+module.exports = {
+    facebookAuth: {
+        "clientID": "facebook clientID",
+        "clientSecret": "facebook clientSecret",
+        "callbackURL": "http://localhost:3000/auth/facebook/callback"
+    },
+    googleAuth: {
+        "clientID": "google clientID",
+        "clientSecret": "google clientSecret",
+        "callbackURL": "http://localhost:3000/auth/google/callback"
+    }
+}
--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,10 @@
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 
 var User            = require('../app/models/user');
+var configAuth = require('./auth');
 
 module.exports = function(passport) {
 
@@ -32,7 +35,7 @@ module.exports = function(passport) {
 				} else {
 					var newUser = new User();
 					newUser.local.username = email;
-					newUser.local.password = password;
+					newUser.local.password = newUser.generateHash(password);
 
 					newUser.save(function(err){
 						if(err)
@@ -57,14 +60,77 @@ module.exports = function(passport) {
 						return done(err);
 					if(!user)
 						return done(null, false, req.flash('loginMessage', 'No User found'));
-					if(user.local.password != password){
-						return done(null, false, req.flash('loginMessage', 'inavalid password'));
+					if(!user.validPassword(password)){
+						return done(null, false, req.flash('loginMessage', 'invalid password'));
 					}
 					return done(null, user);
 
 				});
 			});
 		}
+	));
+
+
+	passport.use(new FacebookStrategy({
+	    clientID: configAuth.facebookAuth.clientID,
+	    clientSecret: configAuth.facebookAuth.clientSecret,
+	    callbackURL: configAuth.facebookAuth.callbackURL
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+	    	process.nextTick(function(){
+	    		User.findOne({'facebook.id': profile.id}, function(err, user){
+	    			if(err)
+	    				return done(err);
+	    			if(user)
+	    				return done(null, user);
+	    			else {
+	    				var newUser = new User();
+	    				newUser.facebook.id = profile.id;
+	    				newUser.facebook.token = accessToken;
+	    				newUser.facebook.name = profile.displayName;
+
+	    				newUser.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, newUser);
+	    				})
+	    				console.log(profile);
+	    			}
+	    		});
+	    	});
+	    }
+
+	));
+
+	passport.use(new GoogleStrategy({
+	    clientID: configAuth.googleAuth.clientID,
+	    clientSecret: configAuth.googleAuth.clientSecret,
+	    callbackURL: configAuth.googleAuth.callbackURL
+	  },
+	  function(accessToken, refreshToken, profile, done) {
+	    	process.nextTick(function(){
+	    		User.findOne({'google.id': profile.id}, function(err, user){
+	    			if(err)
+	    				return done(err);
+	    			if(user)
+	    				return done(null, user);
+	    			else {
+	    				var newUser = new User();
+	    				newUser.google.id = profile.id;
+	    				newUser.google.token = accessToken;
+	    				newUser.google.name = profile.displayName;
+
+	    				newUser.save(function(err){
+	    					if(err)
+	    						throw err;
+	    					return done(null, newUser);
+	    				})
+	    				console.log(profile);
+	    			}
+	    		});
+	    	});
+	    }
+
 	));
 
 
